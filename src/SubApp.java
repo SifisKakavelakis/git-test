4bd232a111d8acd1c48787ff0d0c51d6a9285165
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2025-12-03 13:55:10 +0200

    feat: add subapp

---
+++ src/SubApp.java
@@ -0,0 +1,10 @@
+public class SubApp {
+
+    static void main() {
+        System.out.println(SubApp(10, 5));
+    }
+
+    public static int SubApp(int a, int b) {
+        return a - b;
+    }
+}
